f2d2b78b0b4496017540f08cc07511565b2bb125
Ingrid Gottschalk <igottschalk@charter.net>
2018-04-13 16:23:28 -0500

    added the new columns

--- main.py
+++ main.py
@@ -12,20 +12,28 @@ class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     blog_title = db.Column(db.String(120))
+    blog_entry = db.Column(db.String(120))
 
-    def __init__(self, blog_title):
+    def __init__(self, blog_title, blog_entry):
         self.blog_title = blog_title
+        self.blog_entry = blog_entry
 
-blogs = []
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
     if request.method == 'POST':
-        blog = request.form['blog']
-        blogs.append(blog)
-    
-    return render_template('todos.html',title="Build a Blog", blogs=blogs)
+        blog_add = request.form['blog_title']
+        new_entry = request.form['blog_entry'] 
+
+        new_blog = Blog(blog_add, new_entry)
+
+        db.session.add(new_blog, new_entry)
+        db.session.commit()
+
+    blogs = Blog.query.all()
+    entrys = Blog.query.all()
+    return render_template('todos.html',title="Build a Blog", blogs=blogs, entrys=entrys)
 
 
 if __name__ == '__main__':
